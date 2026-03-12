b02fae7ba8e652bf1335477a45943ea705ca7636
lightlabcreation <lightlabcreation@gmail.com.com>
2026-03-12 17:52:16 +0530

    rohit

--- server.js
+++ server.js
@@ -79,7 +79,16 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
 });
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://accounting-news.kiaantechnology.com',
+        'https://accounting-news.kiaantechnology.com/',
+        'http://localhost:5173'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
